61d20eed8324def865e5aaa11b7c20684399fb3d
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-04-26 13:49:00 -0600

    Update code with Toastr and Router functionalities

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-create-product',
@@ -14,16 +17,18 @@ export class CreateProductComponent implements OnInit {
   /**
    * Constructor with dependency injection.
    * @param formBuilder the instance with the required information of the product.
+   * @param router the instance to redirect the component to other site.
+   * @param toastr the instance to display a message based on Toastr library.
    */
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private router: Router, private toastr: ToastrService) {
     // Creates a new group to set the information of the product based on the web form data
     this.productForm = this.formBuilder.group({
-      
+
       // Establishes each property as required
       name: ['', Validators.required],
       category: ['', Validators.required],
       location: ['', Validators.required],
-      price: ['', Validators.required],
+      price: ['', Validators.required]
     });
   }
 
@@ -34,6 +39,20 @@ export class CreateProductComponent implements OnInit {
    * Method to create a new product based on the information specified in the web form.
    */
   addProduct() {
-    console.log(this.productForm);
+    // Create a product class constant to set the product information
+    const PRODUCT: Product = {
+      name: this.productForm.get('name')?.value,
+      category: this.productForm.get('category')?.value,
+      location: this.productForm.get('location')?.value,
+      price: this.productForm.get('price')?.value
+    };
+
+    console.log(PRODUCT);
+
+    // Displays a message by using the Toastr library 
+    this.toastr.success('Product created successfully', 'Create product');
+
+    // Establishes the target component to redirect the user interaction after a
+    this.router.navigate(['/']);
   }
 }
