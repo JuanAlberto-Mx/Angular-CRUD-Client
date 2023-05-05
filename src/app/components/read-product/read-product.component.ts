2bea3a8d86775f3a4157ff3bcfdc779f8d4183ce
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-05-05 17:56:54 -0600

    Update getProducts method to subscribe the API method available

--- src/app/components/read-product/read-product.component.ts
+++ src/app/components/read-product/read-product.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-read-product',
   templateUrl: './read-product.component.html',
   styleUrls: ['./read-product.component.css']
 })
-export class ReadProductComponent {
+export class ReadProductComponent implements OnInit {
 
+  // Empty array to store the product list
+  listProducts: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  /**
+   * Method to invoke the getProduct method whenever the application start.
+   */
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  /**
+   * Method to subscribe the API methods available.
+   */
+  getProducts() {
+    this.productService.getProducts().subscribe(
+      data => {
+        console.log(data);
+        
+        // Set the listProducts variable to data parameter
+        this.listProducts = data;
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
 }
