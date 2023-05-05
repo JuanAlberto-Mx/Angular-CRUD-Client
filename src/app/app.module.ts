e11df1a7cc974af550e555a7fa752461fe53fb62
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-05-05 17:54:48 -0600

    Add HTTPClient dependencies to communicate with the server application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { CreateProductComponent } from './components/create-product/create-product.component';
-import { ReadProductComponent } from './components/read-product/read-product.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+
+// Components
+import { AppComponent } from './app.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
+import { ReadProductComponent } from './components/read-product/read-product.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +25,8 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule, // Allows including routes
     ReactiveFormsModule, // Add Reactive forms properties
     BrowserAnimationsModule, // Add Browser animations properties from Toastr
-    ToastrModule.forRoot() // Add Toastr module
+    ToastrModule.forRoot(), // Add Toastr module
+    HttpClientModule // Add HTTPClientModule to enable the communication between client and server applications
   ],
   providers: [],
   bootstrap: [AppComponent]
