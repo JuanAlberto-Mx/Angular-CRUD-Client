c20278f64804f4f9649bf88d4ffd3b24da37c31a
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-05-06 18:32:20 -0600

    Add deleteProduct method to the file

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  // Set the URL of the API
+  url = 'http://localhost:4000/api/products';
+
+  // Dependency injection to enable the HTTP communication
+  constructor(private http: HttpClient) {
+    
+  }
+
+  /**
+   * Method to access the GET products method of the API.
+   * Observable allows making asynchronous requests.
+   * @returns the observable reponse.
+   */
+  getProducts(): Observable<any> {
+    return this.http.get(this.url);
+  }
 }
